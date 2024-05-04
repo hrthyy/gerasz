f45279570e839bcf518d541d84d849535a3f6256
hrthyy <167019669+hrthyy@users.noreply.github.com>
2024-05-04 22:53:23 +0200

    Add files via upload

---
+++ 0UAptGWKxO/script.js
@@ -0,0 +1,7 @@
+console.log("--------------------------------")
+console.log("Level 5")
+
+function ask() {
+    document.getElementsByName('body')[0].setAttribute('onclick','');
+    alert('nem szeretem ha L betut irnak joci');
+}

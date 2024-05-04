feca5aa9eceda3935e61ad7853f39761afdd1816
hrthyy <167019669+hrthyy@users.noreply.github.com>
2024-05-04 23:04:16 +0200

    Update script.js

--- 0UAptGWKxO/script.js
+++ 0UAptGWKxO/script.js
@@ -2,6 +2,6 @@ console.log("--------------------------------")
 console.log("Level 5")
 
 function ask() {
-    document.getElementsByName('body')[0].setAttribute('onclick','');
+    document.getElementsByTagName('body')[0].setAttribute('onclick','');
     alert('nem szeretem ha L betut irnak joci');
 }
